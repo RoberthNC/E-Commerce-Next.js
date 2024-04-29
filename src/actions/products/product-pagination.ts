55a837a656dbe8e278205b558d95cb1c5cfd8451
Roberto Núñez <robertonc1599@gmail.com>
2024-04-29 09:39:25 -0500

    Pagination for Main Shop Page

---
+++ src/actions/products/product-pagination.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+  try {
+    // Get all the products
+    const products = await prisma.product.findMany({
+      take: take,
+      skip: (page - 1) * take,
+      include: {
+        ProductImage: {
+          take: 2,
+          select: {
+            url: true,
+          },
+        },
+      },
+    });
+    // TODO: Get total count
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
+    return {
+      currentPage: page,
+      totalPages: totalPages,
+      products: products.map((product) => ({
+        ...product,
+        images: product.ProductImage.map((image) => image.url),
+      })),
+    };
+  } catch (error) {
+    throw new Error("Products could not been loaded");
+  }
+};
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,14 +1,26 @@
-import { initialData } from "@/seed/seed";
-import { ProductGrid } from "@/components";
-import { Title } from "@/components";
+import { ProductGrid, Title, Pagination } from "@/components";
+import { getPaginatedProductsWithImages } from "@/actions";
+import { redirect } from "next/navigation";
 
-const products = initialData.products;
+interface Props {
+  searchParams: {
+    page?: string;
+  };
+}
 
-export default function () {
+export default async function ({ searchParams }: Props) {
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({ page });
+  console.log({ currentPage, totalPages });
+  if (products.length === 0) {
+    redirect("/shop");
+  }
   return (
     <>
       <Title title="Shop" subtitle="All the products" className="mb-2" />
       <ProductGrid products={products} />
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
